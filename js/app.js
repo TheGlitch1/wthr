edd9449f9cffa260a77172b4df6ec62730709bba
Julien <>
2013-10-25 16:34:42 +0200

    Init app.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('deviceready', function(){
+    navigator.splashscreen.hide();
+}, false);
+
+var app = angular.module('app', []);
+
+app.factory('GeolocationService', function($window, $q, $rootScope){
+    var geolocation = $window.navigator.geolocation;
+    return {
+        getCurrentPosition : function(onSuccess, onError){
+            geolocation.getCurrentPosition(function(position){
+                $rootScope.$apply(function(){
+                    onSuccess(position);
+                })
+            }, function(){
+                $rootScope.$apply(function(){
+                    onError();
+                })
+            })
+        }
+    }
+})
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/home', {templateUrl: 'templates/home.html'})
+        .otherwise({redirectTo: '/home'})
+})
